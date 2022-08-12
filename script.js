ca483e50d3cc0d5ab6bf3c96b7fe9432a007acfc
Seth Miller <millerado@gmail.com>
2022-08-12 14:13:51 -0600

    WIP: render list park data

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const API_KEY = 'cPYKs2IvkpGQERkBvONFwDJ8KVSKt1xjb9V3vzXm';
 
 // Cached Element Listeners
 const $form = $('form');
+const $main = $('main');
 const $input = $('input[type="text"]');
 
 // Event Listeners
@@ -26,9 +27,21 @@ function getAllStateParks(event) {
   promise.then(
     (data) => {
       console.log(data);
+      renderParkList(data);
     },
     (error) => {
       console.log(error);
     }
   );
+}
+
+function renderParkList(parkData) {
+  $main.html(`
+  <h3>Parks in ${$input.val()}</h3>
+  <p>${parkData.data[0].fullName}</p>
+  <p>${parkData.data[1].fullName}</p>
+  <p>${parkData.data[2].fullName}</p>
+  <p>${parkData.data[3].fullName}</p>
+  <p>${parkData.data[4].fullName}</p>
+  `);
 }
