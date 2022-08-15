989513cd16be07e4f064e14ad1d9b4cb56afe66b
Seth Miller <millerado@gmail.com>
2022-08-15 07:38:33 -0600

    WIP: added obj w/state abbr to normalize user input

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 // Constants
 const URL = 'https://developer.nps.gov/api/v1';
 const API_KEY = 'cPYKs2IvkpGQERkBvONFwDJ8KVSKt1xjb9V3vzXm';
-
+const stateAbbr = {
+  alabama: 'AL',
+  alaska: 'AK',
+  arizona: 'AZ',
+  arkansas: 'AR',
+  california: 'CA',
+  colorado: 'CO',
+  connecticut: 'CT',
+  delaware: 'DE',
+  florida: 'FL',
+  georgia: 'GA',
+  hawaii: 'HI',
+  idaho: 'ID',
+  illinois: 'IL',
+  indiana: 'IN',
+  iowa: 'IA',
+  kansas: 'KS',
+  kentucky: 'KY',
+  louisiana: 'LA',
+  maine: 'ME',
+  maryland: 'MD',
+  massachusetts: 'MA',
+  michigan: 'MI',
+  minnesota: 'MN',
+  mississippi: 'MS',
+  missouri: 'MO',
+  montana: 'MT',
+  nebraska: 'NE',
+  nevada: 'NV',
+  'new hampshire': 'NH',
+  'new jersey': 'NJ',
+  'new mexico': 'NM',
+  'new york': 'NY',
+  'north carolina': 'NC',
+  'north dakota': 'ND',
+  ohio: 'OH',
+  oklahoma: 'OK',
+  oregon: 'OR',
+  pennsylvania: 'PA',
+  'rhode island': 'RI',
+  'south carolina': 'SC',
+  'south dakota': 'SD',
+  tennessee: 'TN',
+  texas: 'TX',
+  utah: 'UT',
+  vermont: 'VT',
+  virginia: 'VA',
+  washington: 'WA',
+  'west virginia': 'WV',
+  wisconsin: 'WI',
+  wyoming: 'WY',
+};
 // States
 let stateParkList = {};
 
@@ -17,22 +68,12 @@ $ul.on('click', 'li', displayParkInfo);
 $ul.on('click', 'p', displayParkInfo);
 
 // Functions
-function hideDescription(event) {
-  event.target.classList.add('hidden');
-}
-
 function displayParkInfo(event) {
   if (event.target.children.length) {
     event.target.children[1].classList.toggle('hidden');
   } else {
     event.target.classList.add('hidden');
   }
-  // if (!event.target.classList.length || event.target.classList[0] === 'hidden')
-  //   return;
-  // const clickIndex = event.target.classList[0].slice(-1);
-  // const pEl = document.createElement('p');
-  // event.target.append(pEl);
-  // pEl.innerText = `${stateParkList.data[clickIndex].description}`;
 }
 
 function getAllStateParks(event) {
@@ -55,16 +96,12 @@ function getAllStateParks(event) {
     }
   );
 }
-
+// Add city where it's located
 function renderParkList(parkData) {
   $ul.html(`
   <h3>Parks in ${$input.val()}</h3>`);
   parkData.data.forEach((park, index) => {
     const liEl = document.createElement('li');
-    // const pEl = document.createElement('p');
-    // pEl.innerText = `${park.description}`;
-    // pEl.classList.add('hidden');
-    // liEl.append(pEl);
     liEl.innerHTML = `${park.fullName}<br><p class="hidden">${park.description}</p>`;
     liEl.classList.add(`park-${index}`);
     $ul.append(liEl);
